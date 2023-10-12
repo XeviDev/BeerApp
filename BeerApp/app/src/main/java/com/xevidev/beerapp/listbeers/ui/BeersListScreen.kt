efe06194dd136e3cc73ea0ad4829baac52801d79
XeviDev <jv.martinez.mellado@gmail.com>
2023-10-12 18:05:50 +0200

    clearing focus and adding some flavour to the cards

--- BeerApp/app/src/main/java/com/xevidev/beerapp/listbeers/ui/BeersListScreen.kt
+++ BeerApp/app/src/main/java/com/xevidev/beerapp/listbeers/ui/BeersListScreen.kt
@@ -2,7 +2,6 @@ package com.xevidev.beerapp.listbeers.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,7 +11,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -20,8 +18,8 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -32,34 +30,36 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.platform.SoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.xevidev.beerapp.listbeers.domain.model.Beer
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun BeersListScreen(beerListViewModel: BeerListViewModel) {
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val focusManager = LocalFocusManager.current
     Column {
-        searchBar2(beerListViewModel)
+        searchBar2(beerListViewModel, keyboardController)
         val beers by beerListViewModel.beers.collectAsState()
         LazyColumn(
             Modifier
                 .padding(top = 8.dp)
         ) {
             items(beers) { beer ->
-                SingleItem(beer)
+                SingleItem(beer, keyboardController, focusManager)
             }
         }
     }
@@ -69,10 +69,13 @@ fun BeersListScreen(beerListViewModel: BeerListViewModel) {
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun searchBar2(beerListViewModel: BeerListViewModel) {
+fun searchBar2(
+    beerListViewModel: BeerListViewModel,
+    keyboardController: SoftwareKeyboardController?
+) {
     var query by remember { mutableStateOf("") }
     var active by remember { mutableStateOf(false) }
-    val keyboardController = LocalSoftwareKeyboardController.current
+
     TextField(
         modifier = Modifier.fillMaxWidth(),
         shape = RoundedCornerShape(32.dp),
@@ -99,11 +102,22 @@ fun searchBar2(beerListViewModel: BeerListViewModel) {
 
 }
 
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
 @Composable
-fun SingleItem(beer: Beer) {
+fun SingleItem(
+    beer: Beer,
+    keyboardController: SoftwareKeyboardController?,
+    focusManager: FocusManager
+) {
     Card(
         modifier = Modifier.padding(8.dp),
-        colors = CardDefaults.cardColors(contentColor = Color.White, containerColor = Color.Gray)
+        colors = CardDefaults.cardColors(contentColor = Color.White, containerColor = Color.Gray),
+        shape = RoundedCornerShape(12.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+        onClick = {
+            keyboardController?.hide()
+            focusManager.clearFocus()
+        }
     ) {
         Row(
             modifier = Modifier
@@ -127,9 +141,27 @@ fun SingleItem(beer: Beer) {
                 verticalArrangement = Arrangement.SpaceEvenly
             ) {
 
-                Text(text = beer.name, modifier = Modifier.fillMaxWidth(), fontWeight = FontWeight.Bold, overflow = TextOverflow.Ellipsis, maxLines = 1)
-                Text(text = beer.description, overflow = TextOverflow.Ellipsis, maxLines = 2, modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp))
-                Text(text = beer.abv+"% ABV", modifier = Modifier.fillMaxWidth(), overflow = TextOverflow.Ellipsis, maxLines = 1)
+                Text(
+                    text = beer.name,
+                    modifier = Modifier.fillMaxWidth(),
+                    fontWeight = FontWeight.Bold,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1
+                )
+                Text(
+                    text = beer.description,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 2,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 4.dp)
+                )
+                Text(
+                    text = beer.abv + "% ABV",
+                    modifier = Modifier.fillMaxWidth(),
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1
+                )
             }
         }
 
