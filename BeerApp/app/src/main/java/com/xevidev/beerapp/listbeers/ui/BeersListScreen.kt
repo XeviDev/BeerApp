3c6663804c693c3b85d7ebd7d3fa93cf6a20c457
XeviDev <jv.martinez.mellado@gmail.com>
2023-10-12 17:27:19 +0200

    polishing the card and refactor

--- BeerApp/app/src/main/java/com/xevidev/beerapp/listbeers/ui/BeersListScreen.kt
+++ BeerApp/app/src/main/java/com/xevidev/beerapp/listbeers/ui/BeersListScreen.kt
@@ -1,13 +1,18 @@
 package com.xevidev.beerapp.listbeers.ui
 
-import androidx.compose.foundation.border
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -15,12 +20,11 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldColors
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -28,23 +32,32 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.xevidev.beerapp.listbeers.domain.model.Beer
 
 @Composable
 fun BeersListScreen(beerListViewModel: BeerListViewModel) {
-    //searchBar1(beerListViewModel)
     Column {
         searchBar2(beerListViewModel)
         val beers by beerListViewModel.beers.collectAsState()
-        LazyColumn(Modifier.padding(top = 8.dp)) {
+        LazyColumn(
+            Modifier
+                .padding(top = 8.dp)
+        ) {
             items(beers) { beer ->
                 SingleItem(beer)
             }
@@ -86,42 +99,42 @@ fun searchBar2(beerListViewModel: BeerListViewModel) {
 
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun searchBar1(beerListViewModel: BeerListViewModel) {
-    var query by remember { mutableStateOf("") }
-    var active by remember { mutableStateOf(false) }
+fun SingleItem(beer: Beer) {
+    Card(
+        modifier = Modifier.padding(8.dp),
+        colors = CardDefaults.cardColors(contentColor = Color.White, containerColor = Color.Gray)
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(12.dp)
+                .fillMaxSize(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            AsyncImage(
+                model = beer.imageUrl,
+                contentDescription = null,
+                contentScale = ContentScale.Fit,
+                modifier = Modifier
+                    .width(100.dp)
+                    .height(100.dp)
+                    .clip(RoundedCornerShape(16.dp))
+                    .background(Color.White)
 
-    SearchBar(modifier = Modifier.wrapContentHeight(),
-        placeholder = { Text(text = "Search") },
-        leadingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = null) },
-        query = query, onQueryChange = {
-            query = it
-        }, onSearch = {
-            active = false
-        }, active = active, onActiveChange = { active = it }) {
-        beerListViewModel.getSearchBeers(query)
-        val beers by beerListViewModel.beers.collectAsState()
-        LazyColumn {
-            items(beers) { beer ->
-                SingleItem(beer)
+            )
+            Column(
+                modifier = Modifier.padding(start = 12.dp),
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+
+                Text(text = beer.name, modifier = Modifier.fillMaxWidth(), fontWeight = FontWeight.Bold, overflow = TextOverflow.Ellipsis, maxLines = 1)
+                Text(text = beer.description, overflow = TextOverflow.Ellipsis, maxLines = 2, modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp))
+                Text(text = beer.abv+"% ABV", modifier = Modifier.fillMaxWidth(), overflow = TextOverflow.Ellipsis, maxLines = 1)
             }
         }
 
-    }
-}
 
-@Composable
-fun SingleItem(beer: Beer) {
-    Card(Modifier.padding(8.dp)) {
-        AsyncImage(
-            model = beer.imageUrl,
-            contentDescription = null
-        )
-        Text(text = beer.id.toString())
-        Text(text = beer.name)
-        Text(text = beer.description, overflow = TextOverflow.Ellipsis, maxLines = 1)
-        Text(text = beer.tagline)
     }
 
 }
+
